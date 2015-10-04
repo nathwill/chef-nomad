956281e71bd4711468ab1b9d5f336a328c4804bb
Nathan Williams <nath.e.will@gmail.com>
2015-10-04 16:41:03 -0700

    fix ubuntu bootstrap

--- test/fixtures/cookbooks/test/recipes/prepare.rb
+++ test/fixtures/cookbooks/test/recipes/prepare.rb
@@ -1,6 +1,19 @@
 
-package 'docker'
+execute 'apt-get update' do
+  only_if { platform_family?('debian') }
+end
+
+package 'docker' do
+  package_name value_for_platform_family(
+    'debian' => 'docker.io',
+    'default' => 'docker'
+  )
+end
 
 service 'docker' do
+  service_name value_for_platform_family(
+    'debian' => 'docker.io',
+    'default' => 'docker'
+  )
   action [:enable, :start]
 end
