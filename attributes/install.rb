b5e8ce2d639c98a6fb9002ecf16acd4df844fee3
Ryan Slade <ryan@avocet.io>
2016-01-08 10:31:58 +0200

    Bump to 0.2.3

--- attributes/install.rb
+++ attributes/install.rb
@@ -22,18 +22,18 @@ default['nomad'].tap do |nomad|
     case node['os']
     when 'mac_os_x'
       %w(
-        0.2.1/nomad_0.2.1_darwin_amd64.zip
-        7f40f24c7c386bff4f97fad89c258ca7549d9629260d319a71a27fbb9e9ba31f
+        0.2.3/nomad_0.2.3_darwin_amd64.zip
+        ad7caf0641bb7be2aab0dd2496545461010cbc667a857aaac4f10b7296c59ca9
       )
     when 'windows'
       %w(
-        0.2.1/nomad_0.2.1_windows_amd64.zip
-        833fd3fd247ee80f38ed64a6900980833b32012785dc510896df1d4170052768
+        0.2.3/nomad_0.2.3_windows_amd64.zip
+        761996bf3c0296c07b503574cee08e41cb93e1c6412d2927ae0ca6567f91f921
       )
     else
       %w(
-        0.2.1/nomad_0.2.1_linux_amd64.zip
-        a47881efc148cc37a8c94fb120c1d198371b9124e92668e8fc0badddef99cc2a
+        0.2.3/nomad_0.2.3_linux_amd64.zip
+        0f3a7083d160893a291b5f8b4359683c2df7991fa0a3e969f8785ddb40332a8c
       )
     end
 end
