698862dae3af51f2ad3ec64791c8be71b423acb0
Nathan Williams <nath.e.will@gmail.com>
2016-11-09 10:12:58 -0800

    fix chefspec warnings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,10 @@ require 'chefspec/berkshelf'
 
 # Require all our libraries
 Dir.glob('libraries/*.rb').each { |f| require File.expand_path(f) }
+
+RSpec.configure do |config|
+  config.platform = 'centos'
+  config.version = '7.0'
+end
+
+ChefSpec::Coverage.start!
