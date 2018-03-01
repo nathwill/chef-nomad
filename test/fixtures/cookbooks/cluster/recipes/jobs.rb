c5153f8ca169023939bdd8574d6164b956a6e413
Nathan Williams <nath.e.will@gmail.com>
2018-02-28 21:19:58 -0800

    better name

--- test/fixtures/cookbooks/cluster/recipes/jobs.rb
+++ test/fixtures/cookbooks/cluster/recipes/jobs.rb
@@ -1,5 +1,10 @@
+wait = ruby_block 'wait' do
+  block { sleep 10 }
+  action :nothing
+end
 
-nomad_job 'example' do
-  source 'example.hcl.erb'
+nomad_job 'database' do
+  source 'database.hcl.erb'
+  notifies :run, wait.to_s, :delayed # let nomad restart settle
   notifies :run, to_s, :delayed
 end
