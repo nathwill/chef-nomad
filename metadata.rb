13b07643022fe87b0ca60d87fc8d1ae56e5982cd
Nathan Williams <nathan@teamtreehouse.com>
2021-06-01 08:39:35 -0700

    relax chef dep

--- metadata.rb
+++ metadata.rb
@@ -3,8 +3,8 @@ maintainer       'Nathan Williams'
 maintainer_email 'nath.e.will@gmail.com'
 license          'Apache-2.0'
 description      'installs/configures nomad'
-version          '4.0.0'
-chef_version '>= 17.0'
+version          '4.0.1'
+chef_version     '>= 14.15'
 
 gem 'nomad', '~> 0.1'
 
